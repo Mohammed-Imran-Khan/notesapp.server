e95abd63151fab5986daffc0a06c5a0700001a2e
Mohammed-Imran-Khan <127581145+Mohammed-Imran-Khan@users.noreply.github.com>
2023-11-19 12:11:58 +0530

    fetchuser.js

--- Middleware/fetchuser.js
+++ Middleware/fetchuser.js
@@ -1,21 +1,18 @@
 const jwt = require('jsonwebtoken');
 const JWT_SECRET = "imran@123";
 
+const fetchuser = (req, res, next) => {
+  const token = req.header('auth-token');
+  if (!token) {
+    res.status(401).send("Token is Not Valid");
+  }
+  try {
+    const jwtVerification = jwt.verify(token, JWT_SECRET);
+    req.user = jwtVerification.user;
+    next();
+  } catch (error) {
+    res.status(401).send("User not found with this token");
+  }
+};
 
-const fetchuser = (req,res,next) =>{
-    const token = req.header('auth-token');
-    if(!token)
-    {
-        res.status(401).send("Tocken is Not Valid");
-    }
-    try {
-        const jwtverification = jwt.verify(token, JWT_SECRET)
-        req.user = jwtverification.user
-        next();
-    } catch (error) {
-        res.status(401).send("Users not found with this Tokens");
-    }
-}
-
-
-module.exports = fetchuser
+module.exports = fetchuser;
